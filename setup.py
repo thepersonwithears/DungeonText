45a4f9ac435c44fb5fa8b1df9fa4af9fc41a0de0
thepersonwithears <n31ldu@gmail.com>
2024-10-31 11:58:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="dungeon_text_lib",
+    name="dungeontext_lib",
     version="pre-0.1",
     packages=find_packages(),
     description="A simple text adventure game library for Python",
