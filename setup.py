e226eebb1da8282da349874e78db2c2f84d34a40
thepersonwithears <n31ldu@gmail.com>
2024-10-31 11:53:58 -0400

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="dungeon_text_lib",
+    version="pre-0.1",
+    packages=find_packages(),
+    description="A simple text adventure game library for Python",
+    author="thepersonwithears",
+    license="MIT",
+)
