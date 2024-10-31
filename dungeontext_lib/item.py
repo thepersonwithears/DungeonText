574543cc14bad4d2e4463b5a3fb2ad27b36c72c6
thepersonwithears <n31ldu@gmail.com>
2024-10-31 12:00:52 -0400

    Create item.py

---
+++ dungeontext_lib/item.py
@@ -0,0 +1,8 @@
+# item.py
+class Item:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+        return f"{self.name}: {self.description}"
